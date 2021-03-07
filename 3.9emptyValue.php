5e0722375b5f69c2e6a76116c97585294ae61d1c
hossensumon071 <hossensumon071@gmail.com>
2021-03-07 10:45:29 +0600

    empty value in php

---
+++ 3.9emptyValue.php
@@ -0,0 +1,21 @@
+<?php
+
+$name = 0;
+
+if (isset($name)){
+    echo "Name is set";
+} else {
+    echo "not set";
+}
+
+echo "\n";
+
+
+if(empty($name)) {
+    echo "Name is empty";
+} else{
+    echo "not empty";
+}
+
+echo "\n";
+
