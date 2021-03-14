dba4812d80d20c4952789592f52eb19f5f3296af
hossensumon071 <hossensumon071@gmail.com>
2021-03-14 10:50:38 +0600

    array slice in php

---
+++ 3.10arraySlice.php
@@ -0,0 +1,7 @@
+<?php
+$fruits = array('fruits','orange', 'banana', 'mango', 'dates', 'plum');
+
+// $someFruits = array_slice($fruits,2);
+$someFruits = array_slice($fruits,2,2);
+
+print_r($someFruits);
